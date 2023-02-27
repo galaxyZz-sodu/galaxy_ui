bf77872201fefb979606134b4d848e9a1339b2fe
galaxyZz-sodu <1198636978@qq.com>
2023-02-27 15:54:53 +0800

    feat: 改进tag模块

--- src/components/data/Tag.vue
+++ src/components/data/Tag.vue
@@ -2,7 +2,13 @@
   <div>
     <div class="title">标签</div>
     <div class="secondTitle">基础用法</div>
-    <g-tag>你好</g-tag>
+    <div class="out">
+      <g-tag class="item">你好</g-tag>
+      <g-tag class="item" type="success">你好</g-tag>
+      <g-tag class="item" type="warning">你好</g-tag>
+      <g-tag class="item" type="danger">你好</g-tag>
+    </div>
+
     <Code :jsShow="false" :html="basicUsageHTML"></Code>
   </div>
 </template>
@@ -12,11 +18,17 @@ export default {
   data() {
     return {
       basicUsageHTML:
-        '<p>&lt;<span style="color: rgb(224, 108, 117);">g-tag</span>&gt;你好&lt;/<span style="color: rgb(224, 108, 117);">g-tag</span>&gt;</p>',
+        `<p>&lt;<span style="color: rgb(224, 108, 117);">g-tag</span>&gt;你好&lt;/<span style="color: rgb(224, 108, 117);">g-tag</span>&gt;</p><p>&lt;<span style="color: rgb(224, 108, 117);">g-tag</span> <span style="color: rgb(209, 154, 102);">type</span>=<span style="color: rgb(152, 195, 121);">"success"</span>&gt;你好&lt;/<span style="color: rgb(224, 108, 117);">g-tag</span>&gt;</p><p>&lt;<span style="color: rgb(224, 108, 117);">g-tag</span> <span style="color: rgb(209, 154, 102);">type</span>=<span style="color: rgb(152, 195, 121);">"warning"</span>&gt;你好&lt;/<span style="color: rgb(224, 108, 117);">g-tag</span>&gt;</p><p>&lt;<span style="color: rgb(224, 108, 117);">g-tag</span> <span style="color: rgb(209, 154, 102);">type</span>=<span style="color: rgb(152, 195, 121);">"danger"</span>&gt;你好&lt;/<span style="color: rgb(224, 108, 117);">g-tag</span>&gt;</p>`,
     };
   },
 };
 </script>
 
 <style scoped>
+.out {
+  display: flex;
+}
+.item {
+  margin-right: 10px;
+}
 </style>
--- src/galaxyZz-ui/lib/data/tag/index.vue
+++ src/galaxyZz-ui/lib/data/tag/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="out">
-        <div class="in">
+        <div class="in" :class="typeClass">
             <slot></slot>  
         </div>
               
@@ -12,6 +12,17 @@ export default {
     props: {
         type: {
             type: String,
+            default: 'info',
+            validator: (value) => {
+                return ['success', 'info', 'warning', 'danger'].includes(value)
+            }
+        }
+    },
+    computed: {
+        typeClass() {
+            return {
+                [`${this.type}`]: this.type,
+            }
         }
     }
 }
@@ -20,11 +31,23 @@ export default {
 <style scoped>
     .in {
         font-size: 12px;
-        padding: 5px 10px;
+        padding: 6px 10px;
         display: inline-block;
         background-color: #f1f3f6;
         box-shadow:  2px 2px 6px #c6c7ca,
                 -4px -4px 8px #ffffff;
         border-radius: 5px;
+    }
+    .success {
+        background-color: #f0f9eb;
+        color: #67c23a;
+    }
+    .warning {
+        background-color: #fdf6ec;
+        color: #e6a23c;
+    }
+    .danger {
+        background-color: #fef0f0;
+        color: #f56c6c;
     }
 </style>
